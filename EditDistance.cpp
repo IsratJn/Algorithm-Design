875b0bdf48309c7c80c456efa6c171da534202e3
Israt33 <42838747+IsratJn@users.noreply.github.com>
2019-09-05 05:04:08 +0600

    Add files via upload

---
+++ EditDistance.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int min(int x, int y, int z)
+{
+    return min(min(x, y), z);
+}
+
+int editDistDP(string str1, string str2, int m, int n)
+{
+
+    int dp[m+1][n+1];
+
+
+    for (int i=0; i<=m; i++)
+    {
+        for (int j=0; j<=n; j++)
+        {
+
+            if (i==0)
+                dp[i][j] = j;
+            else if (j==0)
+                dp[i][j] = i;
+            else if (str1[i-1] == str2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+
+
+            else
+                dp[i][j] = 1 + min(dp[i][j-1],
+                                   dp[i-1][j],
+                                   dp[i-1][j-1]);
+        }
+    }
+
+    return dp[m][n];
+}
+
+int main()
+{
+   string str1,str2;
+   printf("Enter your 1st string:");
+   cin >> str1;
+    printf("Enter your 2nd string:");
+   cin >> str2;
+    cout << "The leveshtein distance is:";
+    cout << editDistDP(str1, str2, str1.length(), str2.length());
+
+    return 0;
+}
